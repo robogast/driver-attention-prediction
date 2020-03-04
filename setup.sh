d9e04d30d23b5fdf551cb717588e366826ec15a6
robogast <robertjan.schlimbach@gmail.com>
2020-03-04 12:26:11 +0100

    added setup.sh, one more fix for gitignore

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+set -e
+
+# Get script dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Get alexnet weights
+wget -nc --directory-prefix=$DIR https://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
+
+$DIR/prepare_data.sh
