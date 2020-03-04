db141c54748994379c9159e2ea183cbf4a74bfd4
robogast <robertjan.schlimbach@gmail.com>
2020-03-04 15:38:06 +0100

    added manual download note in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ set -e
 # Get script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# TODO get BDDA
+echo "Please download BDDA manually"
+
 # Get alexnet weights
 wget -nc --directory-prefix=$DIR https://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
 
