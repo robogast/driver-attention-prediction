bd79c5855fca16d4e3001105c2cc3dd109c56a43
robogast <robertjan.schlimbach@gmail.com>
2020-03-12 11:21:13 +0100

    added download of models in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # TODO get BDDA
 echo "Please download BDDA manually"
 
+# Get models
+models_zip="pretrained_models.zip"
+wget -nc --directory-prefix=$DIR --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q_CgyX73wrYTAsZjDF9aMXNPURcUmWVy' -O $models_zip
+unzip -u -d $DIR $models_zip
+rm $models_zip
+
 # Get alexnet weights
 wget -nc --directory-prefix=$DIR https://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
 
