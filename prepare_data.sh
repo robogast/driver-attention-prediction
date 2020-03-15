fb7e9feae79aa211768140eb24d483c7100d15a5
Robert Jan Schlimbach <robertjan.schlimbach@gmail.com>
2020-03-15 16:18:21 +0100

    moved every .sh script to LISA spec

--- prepare_data.sh
+++ prepare_data.sh
@@ -4,60 +4,61 @@ set -e
 
 # Get script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATADIR="$TMPDIR"/data
 
 cd $DIR
 
 python parse_videos.py \
---video_dir=data/training/camera_videos \
---image_dir=data/training/camera_images \
+--video_dir="$DATADIR"/training/camera_videos \
+--image_dir="$DATADIR"/training/camera_images \
 --sample_rate=3 \
 --video_suffix=.mp4
 
 python parse_videos.py \
---video_dir=data/training/gazemap_videos \
---image_dir=data/training/gazemap_images \
+--video_dir="$DATADIR"/training/gazemap_videos \
+--image_dir="$DATADIR"/training/gazemap_images \
 --sample_rate=3 \
 --video_suffix=.mp4
 
 python parse_videos.py \
---video_dir=data/validation/camera_videos \
---image_dir=data/validation/camera_images \
+--video_dir="$DATADIR"/validation/camera_videos \
+--image_dir="$DATADIR"/validation/camera_images \
 --sample_rate=3 \
 --video_suffix=.mp4
 
 python parse_videos.py \
---video_dir=data/validation/gazemap_videos \
---image_dir=data/validation/gazemap_images \
+--video_dir="$DATADIR"/validation/gazemap_videos \
+--image_dir="$DATADIR"/validation/gazemap_images \
 --sample_rate=3 \
 --video_suffix=.mp4
 
 python write_tfrecords_for_inference.py \
---data_dir=data/training \
+--data_dir="$DATADIR"/training \
 --n_divides=2 \
 --longest_seq=35
 
 python write_tfrecords_for_inference.py \
---data_dir=data/validation \
+--data_dir="$DATADIR"/validation \
 --n_divides=2 \
 --longest_seq=35
 
 python make_feature_maps.py \
---data_dir=data/training \
+--data_dir="$DATADIR"/training \
 --model_dir=pretrained_models/model_for_inference
 
 python make_feature_maps.py \
---data_dir=data/validation \
+--data_dir="$DATADIR"/validation \
 --model_dir=pretrained_models/model_for_inference
 
 python write_tfrecords.py \
---data_dir=data/training \
+--data_dir="$DATADIR"/training \
 --n_divides=2 \
 --feature_name=alexnet \
 --image_size 288 512 \
 --longest_seq=35
 
 python write_tfrecords.py \
---data_dir=data/training \
+--data_dir="$DATADIR"/training \
 --n_divides=2 \
 --feature_name=alexnet \
 --image_size 288 512 \
--- setup.sh
+++ setup.sh
@@ -2,19 +2,28 @@
 
 set -e
 
-# Get script dir
+# Get script and tmpdata dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TMPDATADIR="$TMPDIR"/data
 
-# TODO get BDDA
-echo "Please download BDDA manually"
+# Prepare and unzip BDDA
+echo "Please download BDDA.zip manually and place in root folder"
+
+mkdir "$TMPDATADIR"
+cp $DIR/BDDA.zip "$TMPDATADIR"
+unzip "$TMPDATADIR"/BDDA.zip '*' -d "$TMPDATADIR" # unzip subfolders into $TMPDATADIR
+
+ls "$TMPDATADIR" # for debugging purposes
 
 # Get models
 models_zip="pretrained_models.zip"
-wget -nc --directory-prefix=$DIR --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q_CgyX73wrYTAsZjDF9aMXNPURcUmWVy' -O $models_zip
-unzip -u -d $DIR $models_zip
+wget -nc --directory-prefix="$DIR" --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q_CgyX73wrYTAsZjDF9aMXNPURcUmWVy' -O $models_zip
+unzip -u -d "$DIR" $models_zip
 rm $models_zip
 
 # Get alexnet weights
-wget -nc --directory-prefix=$DIR https://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
+wget -nc --directory-prefix="$DIR" https://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
 
 $DIR/prepare_data.sh
+
+cp -r "$TMPDATADIR"/*  "$DIR"/data
